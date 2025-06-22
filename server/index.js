ce2cf1b31bb27ee66719f988136f3176ebbda761
ekanshgouttam <egouttam@gmail.com>
2025-06-22 14:51:16 +0530

    fix static file path for render deployment

--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ app.use('/api/tasks', taskRoutes); // You can comment this out if `taskRoutes` d
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/dist')));
+  app.use(express.static(path.join(__dirname, '..client/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '..client/dist/index.html'));
   });
 }
 
